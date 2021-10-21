fdab257dcf8b5167c2820cf37cfb82eb5c3bbef1
ahmed-idenbrid <84811071+ahmed-idenbrid@users.noreply.github.com>
2021-10-21 12:54:58 +0500

    new changes

--- client/src/app/common.js
+++ client/src/app/common.js
@@ -1,3 +1,1 @@
-// export const BaseURL = "https://pakteki.herokuapp.com";
-export const BaseURL = "http://localhost:5000/api";
-// export const BaseURL = "http://3.142.50.232:5000/api";
+export const BaseURL = "http://localhost:5000";
